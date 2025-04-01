d3ab7e597722c923d31040201b4e6e24ab9b4bf2
GabrielGuevaraLopez <114413163+GabrielGuevaraLopez@users.noreply.github.com>
2025-04-02 01:58:37 +0200

    updated style for decorator pattern

--- JabberPointSQ/src/main/java/org/nhlstenden/jabberpoint/decorator/Style.java
+++ JabberPointSQ/src/main/java/org/nhlstenden/jabberpoint/decorator/Style.java
@@ -20,6 +20,21 @@ public class Style implements StyleComponent {
         this.leading = leading;
     }
 
+    public static Style getStyle(int level) {
+        switch (level) {
+            case 0:
+                return new Style(0, Color.red, 48, 20);
+            case 1:
+                return new Style(20, Color.blue, 40, 18);
+            case 2:
+                return new Style(40, Color.black, 36, 16);
+            case 3:
+                return new Style(60, Color.darkGray, 30, 14);
+            default:
+                return new Style(80, Color.gray, 24, 12);
+        }
+    }
+
     @Override
     public void createStyles() {
         System.out.println("Base style created: " + this.toString());
